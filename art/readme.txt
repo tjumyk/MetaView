87ca99cb69d8b5c8eac79469789c6a89e910c00c
tjumyk <tjumyk@gmail.com>
2014-05-30 12:14:39 +0800

    add help images, add i18n support for help images.

---
+++ art/readme.txt
@@ -0,0 +1,4 @@
+Softwares for editing artwork files:
+
+*.snag --- Snagit Editor
+*.ep   --- Pencil (GUI prototyping tool)
--- src/org/tjumyk/metaview/controller/DialogHelpController.java
+++ src/org/tjumyk/metaview/controller/DialogHelpController.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
-import org.tjumyk.metaview.Main;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Pagination;
@@ -14,6 +12,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+
+import org.tjumyk.metaview.Main;
 
 /**
  * Controller of the "Help" dialog.
@@ -28,14 +28,24 @@ public class DialogHelpController extends DialogControllerBase {
 	private Pagination pagination_help_image;
 
 	/**
-	 * Parent folder of the help images
+	 * Parent folder of all the help images
 	 */
-	private static final String IMAGE_PATH = "help-images/";
+	private static final String IMAGE_BASE_PATH = "help-images/";
 
 	/**
 	 * Total count of the help images
 	 */
 	private static final int IMAGE_TOTAL = 5;
+
+	/**
+	 * fit height of help images
+	 */
+	private static final int IMAGE_FIT_HEIGHT = 300;
+
+	/**
+	 * fit width of help images
+	 */
+	private static final int IMAGE_FIT_WIDTH = 540;
 
 	/**
 	 * All the loaded ImageView list
@@ -44,9 +54,15 @@ public class DialogHelpController extends DialogControllerBase {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		String dir = "images/" + IMAGE_BASE_PATH
+				+ Main.getString("help.image_dir") + "/";
 		for (int i = 1; i <= IMAGE_TOTAL; i++) {
-			imgList.add(new ImageView(new Image(Main.class
-					.getResourceAsStream("images/" + IMAGE_PATH + i + ".png"))));
+			ImageView imgView = new ImageView(new Image(
+					Main.class.getResourceAsStream(dir + i + ".png")));
+			imgView.setFitHeight(IMAGE_FIT_HEIGHT);
+			imgView.setFitWidth(IMAGE_FIT_WIDTH);
+			imgView.setPreserveRatio(true);
+			imgList.add(imgView);
 		}
 		pagination_help_image.setPageCount(IMAGE_TOTAL);
 		pagination_help_image.setMaxPageIndicatorCount(IMAGE_TOTAL);
